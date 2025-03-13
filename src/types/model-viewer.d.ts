6f1622cd094ad8686204fd8a118a33cf1b1b28d7
darklift <darklift@gmail.com>
2025-03-13 13:31:47 +0100

    typefixes

---
+++ src/types/model-viewer.d.ts
@@ -0,0 +1,18 @@
+// src/types/model-viewer.d.ts
+
+// Define the custom element
+declare namespace JSX {
+  interface IntrinsicElements {
+    'model-viewer': React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement> & {
+        src?: string;
+        alt?: string;
+        'camera-controls'?: boolean;
+        'auto-rotate'?: boolean;
+        id?: string;
+        style?: React.CSSProperties;
+      },
+      HTMLElement
+    >;
+  }
+}
