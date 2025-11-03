26aa8b85fed4d24cf06ef400a2b2fe3151cad21c
darklift <darklift@gmail.com>
2025-11-03 17:02:21 +0100

    Update three-module.d.ts

--- types/three-module.d.ts
+++ types/three-module.d.ts
@@ -1,7 +1,7 @@
 // Type shim for runtime-loaded local Three.js ESM at /three.module.js
 // We re-export typings from the npm 'three' package so TS can infer types.
 declare module '/three.module.js' {
-  export * from 'three'; 
+  export * from 'three';
 }
 
 
